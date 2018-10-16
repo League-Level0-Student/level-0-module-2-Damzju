e5eba97c304cb3c97fe20411f4a1805a49e2df43
55378 <55378@0338S-S-R0B6ZH0.sdcs.local>
2018-10-16 14:26:26 -0700

    but no

--- src/random/Magic8Ball.java
+++ src/random/Magic8Ball.java
@@ -8,8 +8,11 @@ import java.util.Random;
 
 public class Magic8Ball {
 
-	// 1. Make a main method that includes all the steps below….
-
+	// 1. Make a main method that includes all the steps below.
+public static void main(String[] args) {
+	Random randomMaker = new Random();
+	int randomNumber = 
+}
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
 
 	// 3. Print out this variable
