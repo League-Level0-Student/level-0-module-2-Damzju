2f72dd494e4644fe9596e5541f3bfe38d89b0742
55378 <55378@0338S-S-R0B6ZH0.sdcs.local>
2018-10-23 14:26:18 -0700

    ree

--- src/random/Magic8Ball.java
+++ src/random/Magic8Ball.java
@@ -6,19 +6,23 @@ package random;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
+	public static void main(String[] args) {
+		Random randomMaker = new Random();
+		int randomNumber =  new Random().nextInt(4);
+		}
 	// 1. Make a main method that includes all the steps below.
-public static void main(String[] args) {
-	Random randomMaker = new Random();
-	int randomNumber = 
-}
-	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
 
+	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
+	
 	// 3. Print out this variable
+System.out.println(randomNumber);
 
 	// 4. Get the user to enter a question for the 8 ball
-
+JOptionPane.showInputDialog("Enter Question for Magic Ball Boi");
 	// 5. If the random number is 0
 
 	// -- tell the user "Yes"
